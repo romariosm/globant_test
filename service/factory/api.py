2210aee15a02ec52c128e8a72d1e7550e02b1e1e
rsanchez <romariosm13@gmail.com>
2020-11-17 09:26:41 -0500

    comments added

--- service/factory/api.py
+++ service/factory/api.py
@@ -1,7 +1,6 @@
 from enum import Enum
 import requests
 import xmltodict
-import logging
 import json
 
 
@@ -13,6 +12,9 @@ class Method(Enum):
 
 
 class Service:
+    """
+    service definition class
+    """
     def __init__(self, name, base_url):
         self.__name = name
         self.__base_url = base_url
@@ -22,6 +24,9 @@ class Service:
 
 
 class ServiceAPI:
+    """
+    API service consumer
+    """
     def __init__(self, name, service, method, params, http_headers):
         self.__name = name
         self.__service = service
@@ -31,7 +36,7 @@ class ServiceAPI:
 
     def validate_params(self, **kwargs):
         """
-
+        Validates params sent are correct according to its type
         """
         for param in self.__params:
             if not param.is_valid(kwargs.get(param.get_name)):
@@ -40,7 +45,6 @@ class ServiceAPI:
     def execute(self, **kwargs):
         """
         Executes service to retrieve data
-
         """
         self.validate_params(**kwargs)
         if self.__method == Method.GET.name:
